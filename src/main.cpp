c52d0152a6974fd634563c0f06477e677254e398
Niall Lenihan <engenious23@gmail.com>
2023-01-07 01:04:19 +1300

    Add skeleton main
    No actual code yet, just the skeleton and required cmake directives.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// SPDX-License-Identifier: MIT
+// Copyright (c) Contributors to the elf_viewer Project.
+//
+
+#include <cstdlib>
+
+#include "VersionInfo.h"
+
+int main( int i_argc, const char** i_argv )
+{
+    return EXIT_SUCCESS;
+}
